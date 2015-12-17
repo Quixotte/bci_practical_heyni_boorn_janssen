139ff3cae0004c052299b0c404ea520ac444edc4
Boorn <hgvandenboorn@student.ru.nl>
2015-12-17 13:24:42 +0100

    More work in PCA, it works but it needs more analysis for reconstructing the original datapoints

--- feature_extraction.m
+++ feature_extraction.m
@@ -9,17 +9,18 @@ S = size(datapoint,3);
 T = 1/Fs;             % Sampling period
 t = (0:L-1)*T;        % Time vector
 %nyquist = L/2;
+
 for sample=1:S/2
-    datapoint(:,:,sample) = 0.7*sin(2*pi*[50;50;51;52;48;41;50;50;50;50]*t)+2*randn(C,L);
+    datapoint(:,:,sample) = 0.7*rand(C,L).*sin(2*pi*[50;50;51;52;48;41;50;50;50;50]*t)+2*randn(C,L);
 end
 for sample=S/2+1:S
-    datapoint(:,:,sample) = 0.7*sin(2*pi*[20;20;21;22;18;11;20;20;20;20]*t)+2*randn(C,L);
+    datapoint(:,:,sample) = 0.7*rand(C,L).*sin(2*pi*[20;20;21;22;18;11;20;20;20;20]*t)+2*randn(C,L);
 end
 
 P1 = zeros(S,C,L/2+1);
 for s=1:S
     for i=1:C
-        Y = fft(datapoint(i,:));
+        Y = fft(datapoint(:,:,s));
         P2 = abs(Y./L);
         P1(s,i,:) = 2*P2(1:L/2+1);
         P1(s,i,2:end-1) = 2*P1(s,i,2:end-1);
@@ -28,8 +29,13 @@ end
 f = Fs*(0:(L/2))/L;
 
 IND = find(f<=45);
-plot(f(IND),P1(:,IND));
 P1 = P1(:,:,IND);
 Data = P1(:,:);
+classlabels = zeros(S,1);
+classlabels(1:S/2) = 1;
+classlabels(S/2+1:end) = 2;
+Data = [Data classlabels];
 
-[COEFF,score,latent] = princomp(Data);
+[coeff1,score1,latent] = princomp(Data);
+A=cumsum(latent)./sum(latent);
+vectors = score(:,A<0.95);
