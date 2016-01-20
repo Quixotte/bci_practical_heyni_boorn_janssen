1d8c03b59f5bb660ac491ec2714f442d6de393f9
Thomas van Heyningen <Thomas_vh@Live.nl>
2016-01-20 15:36:26 +0100

    Pipeline

---
+++ testFunction.m
@@ -0,0 +1,27 @@
+try; cd(fileparts(mfilename('fullpath')));catch; end;
+run buffer_bci-master/utilities/initPaths.m
+
+buffhost='localhost';
+buffport=1972;
+trlen_samp = 50; % #samples per epoch
+
+phaseToRun=[];
+while ( isempty(phaseToRun) || ~isstruct(phaseToRun) || (phaseToRun.nchans==0) ) % wait for the buffer to contain valid data
+  try 
+    phaseToRun=buffer('get_hdr',[],buffhost,buffport); 
+  catch
+    phaseToRun=[];
+    fprintf('Invalid header info... waiting.\n');
+  end;
+  pause(1);
+end;
+
+initgetwTime;
+initsleepSec;
+state = [];
+endTest = false;
+
+while(true)
+    sendEvent('stimulus.epoch','start');
+    pause(1);
+end
